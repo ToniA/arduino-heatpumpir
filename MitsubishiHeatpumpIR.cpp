a3b79acdb8671eac21c9d1e53fdde8eefdf39e97
ToniA <ToniA@server.fake>
2013-12-30 17:46:17 +0200

    Experimental MItsubishi air direction support

--- MitsubishiHeatpumpIR.cpp
+++ MitsubishiHeatpumpIR.cpp
@@ -21,10 +21,12 @@ void MitsubishiHeatpumpIR::send(IRSender& IR, byte powerModeCmd, byte operatingM
 {
   // Sensible defaults for the heat pump mode
 
-  byte powerMode = MITSUBISHI_AIRCON1_MODE_ON;
+  byte powerMode     = MITSUBISHI_AIRCON1_MODE_ON;
   byte operatingMode = MITSUBISHI_AIRCON1_MODE_HEAT;
-  byte fanSpeed = MITSUBISHI_AIRCON1_FAN_AUTO;
-  byte temperature = 23;
+  byte fanSpeed      = MITSUBISHI_AIRCON1_FAN_AUTO;
+  byte temperature   = 23;
+  byte swingV        = MITSUBISHI_AIRCON1_VS_AUTO;
+  byte swingH        = MITSUBISHI_AIRCON1_HS_SWING;
 
   if (powerModeCmd == 0)
   {
@@ -46,9 +48,14 @@ void MitsubishiHeatpumpIR::send(IRSender& IR, byte powerModeCmd, byte operatingM
       operatingMode = MITSUBISHI_AIRCON1_MODE_DRY;
       break;
     case MODE_FAN:
-      operatingMode = MITSUBISHI_AIRCON1_MODE_COOL;
-      // Temperature needs to be set to 31 degrees for 'simulated' FAN mode
-      temperatureCmd = 31;
+      if (_mitsubishiModel == MITSUBISHI_FE) {
+        operatingMode = MITSUBISHI_AIRCON1_MODE_FAN;
+        temperatureCmd = 24;
+      } else {
+        operatingMode = MITSUBISHI_AIRCON1_MODE_COOL;
+        // Temperature needs to be set to 31 degrees for 'simulated' FAN mode
+        temperatureCmd = 31;
+      }
       break;
     case MODE_MAINT: // Maintenance mode is just the heat mode at +10, FAN5
       if (_mitsubishiModel == MITSUBISHI_FE) {
@@ -83,12 +90,60 @@ void MitsubishiHeatpumpIR::send(IRSender& IR, byte powerModeCmd, byte operatingM
     temperature = temperatureCmd;
   }
 
-  sendMitsubishi(IR, powerMode, operatingMode, fanSpeed, temperature, 0, 0);
+  switch (swingVCmd)
+  {
+    case VDIR_AUTO:
+      swingV = MITSUBISHI_AIRCON1_VS_AUTO;
+      break;
+    case VDIR_SWING:
+      swingV = MITSUBISHI_AIRCON1_VS_SWING;
+      break;
+    case VDIR_UP:
+      swingV = MITSUBISHI_AIRCON1_VS_UP;
+      break;
+    case VDIR_MUP:
+      swingV = MITSUBISHI_AIRCON1_VS_MUP;
+      break;
+    case VDIR_MIDDLE:
+      swingV = MITSUBISHI_AIRCON1_VS_MIDDLE;
+      break;
+    case VDIR_MDOWN:
+      swingV = MITSUBISHI_AIRCON1_VS_MDOWN;
+      break;
+    case VDIR_DOWN:
+      swingV = MITSUBISHI_AIRCON1_VS_DOWN;
+      break;
+  }
+
+  switch (swingHCmd)
+  {
+    case HDIR_AUTO:
+    case HDIR_SWING:
+      swingH = MITSUBISHI_AIRCON1_HS_SWING;
+      break;
+    case HDIR_MIDDLE:
+      swingH = MITSUBISHI_AIRCON1_HS_MIDDLE;
+      break;
+    case HDIR_LEFT:
+      swingH = MITSUBISHI_AIRCON1_HS_LEFT;
+      break;
+    case HDIR_MLEFT:
+      swingH = MITSUBISHI_AIRCON1_HS_MLEFT;
+      break;
+    case HDIR_RIGHT:
+      swingH = MITSUBISHI_AIRCON1_HS_RIGHT;
+      break;
+    case HDIR_MRIGHT:
+      swingH = MITSUBISHI_AIRCON1_HS_MRIGHT;
+      break;
+  }
+
+  sendMitsubishi(IR, powerMode, operatingMode, fanSpeed, temperature, swingV, swingH);
 }
 
-void MitsubishiHeatpumpIR::sendMitsubishi(IRSender& IR, byte powerMode, byte operatingMode, byte fanSpeed, byte temperature, byte swingVCmd, byte swingHCmd)
+void MitsubishiHeatpumpIR::sendMitsubishi(IRSender& IR, byte powerMode, byte operatingMode, byte fanSpeed, byte temperature, byte swingV, byte swingH)
 {
-  byte MitsubishiTemplate[] = { 0x23, 0xCB, 0x26, 0x01, 0x00, 0x20, 0x48, 0x00, 0xC0, 0x7A, 0x61, 0x00, 0x00, 0x00, 0x10, 0x40, 0x00, 0x00 };
+  byte MitsubishiTemplate[] = { 0x23, 0xCB, 0x26, 0x01, 0x00, 0x20, 0x48, 0x00, 0x00, 0x00, 0x61, 0x00, 0x00, 0x00, 0x10, 0x40, 0x00, 0x00 };
   //                            0     1     2     3     4     5     6     7     8     9     10    11    12    13    14    15    16    17
 
   byte checksum = 0x00;
@@ -105,8 +160,11 @@ void MitsubishiHeatpumpIR::sendMitsubishi(IRSender& IR, byte powerMode, byte ope
     MitsubishiTemplate[7] = temperature - 16;
   }
 
-  // Set the operatingmode on the template message
-  MitsubishiTemplate[9] = fanSpeed;
+  // Set the horizontal air direction on the template message
+  MitsubishiTemplate[8] = swingH;
+
+  // Set the fan speed and vertical air direction on the template message
+  MitsubishiTemplate[9] = fanSpeed | swingV;
 
   // Calculate the checksum
   for (int i=0; i<17; i++) {
