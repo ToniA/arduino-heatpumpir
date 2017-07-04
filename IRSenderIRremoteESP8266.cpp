1dd19d8305896b805bf741d365da2387f6c17049
ToniA <ToniA@users.noreply.github.com>
2017-07-04 09:51:34 +0300

    Use the 'IRsend' implementation from the IRremoteESP8266 library on ESP8266

---
+++ IRSenderIRremoteESP8266.cpp
@@ -0,0 +1,22 @@
+#include <Arduino.h>
+#include <IRSender.h>
+
+IRSenderIRremoteESP8266::IRSenderIRremoteESP8266(uint8_t pin) : IRSender(pin), _ir(pin) 
+{
+  _ir.begin();
+}
+
+void IRSenderIRremoteESP8266::setFrequency(int frequency)
+{
+  _ir.enableIROut(frequency);
+}
+
+void IRSenderIRremoteESP8266::space(int spaceLength)
+{
+  _ir.space(spaceLength);
+}
+
+void IRSenderIRremoteESP8266::mark(int markLength)
+{
+  _ir.mark(markLength);
+}
